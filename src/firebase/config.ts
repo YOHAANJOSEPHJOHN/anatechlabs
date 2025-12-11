e440ff9b635116335c223924edbe0f5017e99a20
YOHAANJOSEPHJOHN <yohanjosephjohn@gmail.com>
2025-12-11 12:47:05 +0530

    Delete src directory

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-37422489-bf53f",
-  "appId": "1:224236283431:web:c9f01e275b345bc830d002",
-  "apiKey": "AIzaSyAZWGMPBfK9mtsB7Gg0WnVucfOMfThQgkA",
-  "authDomain": "studio-37422489-bf53f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "224236283431",
-  "storageBucket": "studio-37422489-bf53f.appspot.com"
-};
