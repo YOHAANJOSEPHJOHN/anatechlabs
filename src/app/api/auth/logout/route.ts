b986f8a3026766f3aae55ac759c43fe6896e9ab4
YOHAANJOSEPHJOHN <yohanjosephjohn@gmail.com>
2026-01-12 15:37:26 +0530

    Delete src directory

--- src/app/api/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-
-import { deleteSession } from '@/lib/session';
-import { redirect } from 'next/navigation';
-import { NextRequest } from 'next/server';
-
-export async function POST(request: NextRequest) {
-  await deleteSession();
-  redirect('/login');
-}
