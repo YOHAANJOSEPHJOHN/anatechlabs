9d68b47f20344e7981cb7ad365dc0b9d34a93e90
YOHAANJOSEPHJOHN <yohanjosephjohn@gmail.com>
2026-01-29 15:51:38 +0530

    Delete src directory

--- src/firebase/error-emitter.ts
+++
@@ -1,6 +0,0 @@
-
-import { EventEmitter } from 'events';
-
-// Since we're in a single-process environment (browser/server), a simple EventEmitter is fine.
-// For multi-process or serverless environments, you might need a more robust pub/sub system.
-export const errorEmitter = new EventEmitter();
